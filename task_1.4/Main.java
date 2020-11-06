76205fb62ba0ec4eb4c999d24c695ecd2271c735
Roman <tgusakov@gmail.com>
2020-11-06 15:50:15 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -1,27 +1,9 @@
-/*
-ВНИМАНИЕ! ДЛЯ РЕШЕНИЯ ЗАДАЧИ НЕОБХОДИМО ПРОСМОТРЕТЬ УРОК №4 https://www.youtube.com/watch?v=WmZ6kVTlWtg&list=PLOep0pi-UBExIxCNegkvCrBuDh0TE7Ihq&index=4
-Программа запрашивает у пользователя значение в градусах Цельсия. Программа должена переводить температуру и выводить на экран
-значение в градусах Фаренгейта.
-Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношением:
-TF = (9 / 5) * TC + 32
-Пример:
-на вход подается значение 41.
-Пример вывода:
-105.8 */
-
-import java.util.Scanner;
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
 
 public class Main {
     public static void main(String[] args) {
-        double tempCels;
-        double tempFar;
-
-        Scanner enterTemperature = new Scanner(System.in);
-
-        tempCels = enterTemperature.nextDouble();
-
-        tempFar = (double) 9 / 5 * tempCels + 32;
-
-        System.out.println(tempFar);
+        int number = 25;
+        System.out.println(number * number);
     }
 }
