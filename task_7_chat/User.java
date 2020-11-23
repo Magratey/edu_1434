8b241afe7d3814a6efc6ad2f7ec4415a258695c1
Roman <tgusakov@gmail.com>
2020-11-23 15:19:47 +0300

    Create User.java

---
+++ task_7_chat/User.java
@@ -0,0 +1,27 @@
+import java.net.Socket;
+
+public class User {
+    private String name;
+    private Socket socket;
+
+    public User(String name, Socket socket) {
+        this.name = name;
+        this.socket = socket;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+}
